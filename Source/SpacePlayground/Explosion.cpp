c10d375853e83d631c77f1e9e980c4e5b64c71c9
Joel Lopez <jowie.jowie@gmail.com>
2017-02-01 21:42:30 +0100

    Add basic explosion code and particles

---
+++ Source/SpacePlayground/Explosion.cpp
@@ -0,0 +1,57 @@
+// All rights Neebula Games
+
+#include "SpacePlayground.h"
+#include "Explosion.h"
+
+
+// Sets default values
+AExplosion::AExplosion()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BillboardComponent = CreateDefaultSubobject<UBillboardComponent>(TEXT("Billboard"));
+	BillboardComponent->SetupAttachment(RootComponent);
+
+	RadialForceComponent = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForce"));
+	RadialForceComponent->SetupAttachment(BillboardComponent);
+	RadialForceComponent->Radius = 1000.f;
+	RadialForceComponent->ImpulseStrength = 500000.f;
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> explosionPS(TEXT("ParticleSystem'/Game/Particles/P_Explosion.P_Explosion'"));
+
+	ParticleSystemComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleSystem"));
+	ParticleSystemComponent->SetupAttachment(BillboardComponent);
+	ParticleSystemComponent->SetTemplate(explosionPS.Object);
+	ParticleSystemComponent->bAutoActivate = false;
+
+	static ConstructorHelpers::FObjectFinder<USoundWave> explosionSound(TEXT("SoundWave'/Game/Sounds/Explosion01.Explosion01'"));
+	this->explosionSound = explosionSound.Object;
+}
+
+// Called when the game starts or when spawned
+void AExplosion::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FTimerHandle unusedHandle;
+	GetWorldTimerManager().SetTimer(unusedHandle, this, &AExplosion::DelayedExplosion, 2.f);
+}
+
+// Called every frame
+void AExplosion::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+void AExplosion::DelayedExplosion()
+{
+	RadialForceComponent->FireImpulse();
+
+	ParticleSystemComponent->Activate();
+
+	
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), explosionSound, FVector(0.f));
+}
+
